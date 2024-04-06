401ab3f2c2cf5d9db4db800b8c0c37ffa07a7655
Christopher Carson <chris@nowzoo.com>
2024-04-06 17:41:12 -0500

    confirm that it is a bug only in svelte 5

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,39 @@
 <script lang="ts">
 	import Modal from './Modal.svelte';
 
-	let modalOpen = $state(false);
-  let duration: number = $state(500);
+	let modalOpen = false;
+  let duration: number = 500;
   const close = () => {
     modalOpen = false;
   }
 </script>
 
-<div class="container mx-auto my-8 px-4">
+<div class="max-w-lg mx-auto my-8 px-4">
 	<div class="space-y-4">
-		<h1>Welcome to SvelteKit</h1>
-		<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+		<h1 class="text-2xl">Svelte 4.2.12</h1>
 
     <p>
-      Tranistion Duration: <input type="number" class="bg-white text-black" bind:value={duration}/>
+      Open up dev tools and look at the transition values (i.e. the translate y %) when you open or close the modal. In Svelte 4 
+      the last value is always correct (0 or 100.) 
+    </p>
+
+ 
+		
+    <p>
+      Transition Duration: <input type="number" class="bg-white text-black" bind:value={duration}/>
     </p>
 		<p>
-			<button class="underline" onclick={() => modalOpen = true}>Open Modal</button>
+			<button class="underline text-5xl" on:click={() => modalOpen = true}>Open Modal</button>
 		</p>
+
+    <p>
+      To see the bad behavior in Svelte 5 do:
+    </p>
+    <pre>
+      git switch main
+      pnpm i
+      pnpm dev
+    </pre>
 	</div>
 </div>
 
--- src/routes/Modal.svelte
+++ src/routes/Modal.svelte
@@ -1,55 +1,49 @@
 <script lang="ts">
-  import { sineInOut } from 'svelte/easing';
-  type P = {
-    close: () => void;
-    duration: number;
-  };
-  let { close, duration  }: P = $props();
-  
+	import { sineInOut } from 'svelte/easing';
+	export let close: () => void;
+	export let duration: number;
 
-  const modalSlide = (_node: HTMLElement, { duration = 500 }) => {
-    const mediaQuery = window.matchMedia('(prefers-reduced-motion: reduce)');
-    const calculatedDuration = mediaQuery && mediaQuery.matches ? 0 : duration;
-    return {
-      delay: 0,
-      duration: calculatedDuration,
-      css: (t: number) => {
-        let tyPct = 100 - sineInOut(t) * 100;
-        console.log(tyPct)
-        // if (tyPct > 99.5) {
-        //   tyPct = 100
-        // } else if (tyPct < .5) {
-        //   tyPct = 0;
-        // }
-        return `transform: translateY(${tyPct}%)`;
-      }
-    };
-  };
-  
-  let containerRect = $state();
-  const handleKeydown = (event: KeyboardEvent) => {
-    if ('Escape' === event.key) {
-      close();
-    }
-  };
+	const modalSlide = (_node: HTMLElement, { duration = 500 }) => {
+		const mediaQuery = window.matchMedia('(prefers-reduced-motion: reduce)');
+		const calculatedDuration = mediaQuery && mediaQuery.matches ? 0 : duration;
+		return {
+			delay: 0,
+			duration: calculatedDuration,
+			css: (t: number) => {
+				let tyPct = 100 - sineInOut(t) * 100;
+				console.log(tyPct);
+				// if (tyPct > 99.5) {
+				//   tyPct = 100
+				// } else if (tyPct < .5) {
+				//   tyPct = 0;
+				// }
+				return `transform: translateY(${tyPct}%)`;
+			}
+		};
+	};
+
+	const handleKeydown = (event: KeyboardEvent) => {
+		if ('Escape' === event.key) {
+			close();
+		}
+	};
 </script>
-<svelte:window onkeydown={handleKeydown}/>
-<div bind:contentRect={containerRect}  class="modal" transition:modalSlide|global={{ duration }}>
-<pre>{JSON.stringify(containerRect, null, 1)}</pre>
-<button type="button" class="underline" onclick={close}>Close</button> (or press <kbd>esc</kbd>)
+
+<svelte:window on:keydown={handleKeydown} />
+<div class="modal" transition:modalSlide|global={{ duration }}>
+	<button type="button" class="underline" on:click={close}>Close</button> (or press <kbd>esc</kbd>)
 </div>
 
 <style lang="postcss">
- .modal {
-  position: fixed;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  z-index: 1;
-  background-color: #fff;
-  color: #000;
-  padding: 1rem;
- }
-  
+	.modal {
+		position: fixed;
+		top: 0;
+		bottom: 0;
+		left: 0;
+		right: 0;
+		z-index: 1;
+		background-color: #fff;
+		color: #000;
+		padding: 1rem;
+	}
 </style>
