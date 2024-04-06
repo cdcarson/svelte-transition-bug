0f94098067c4d04217ffa9db07db67ec15d86ae2
Christopher Carson <chris@nowzoo.com>
2024-04-06 17:20:54 -0500

    svelte 5

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Modal from './Modal.svelte';
+
+	let modalOpen = $state(false);
+  let duration: number = $state(500);
+  const close = () => {
+    modalOpen = false;
+  }
+</script>
+
+<div class="container mx-auto my-8 px-4">
+	<div class="space-y-4">
+		<h1>Welcome to SvelteKit</h1>
+		<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+    <p>
+      Tranistion Duration: <input type="number" class="bg-white text-black" bind:value={duration}/>
+    </p>
+		<p>
+			<button class="underline" onclick={() => modalOpen = true}>Open Modal</button>
+		</p>
+	</div>
+</div>
+
+{#if modalOpen}
+	<Modal {close} {duration} />
+{/if}
---
+++ src/routes/Modal.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+  import { sineInOut } from 'svelte/easing';
+  type P = {
+    close: () => void;
+    duration: number;
+  };
+  let { close, duration  }: P = $props();
+  
+
+  const modalSlide = (_node: HTMLElement, { duration = 500 }) => {
+    const mediaQuery = window.matchMedia('(prefers-reduced-motion: reduce)');
+    const calculatedDuration = mediaQuery && mediaQuery.matches ? 0 : duration;
+    return {
+      delay: 0,
+      duration: calculatedDuration,
+      css: (t: number) => {
+        let tyPct = 100 - sineInOut(t) * 100;
+        console.log(tyPct)
+        // if (tyPct > 99.5) {
+        //   tyPct = 100
+        // } else if (tyPct < .5) {
+        //   tyPct = 0;
+        // }
+        return `transform: translateY(${tyPct}%)`;
+      }
+    };
+  };
+  
+  let containerRect = $state();
+  const handleKeydown = (event: KeyboardEvent) => {
+    if ('Escape' === event.key) {
+      close();
+    }
+  };
+</script>
+<svelte:window onkeydown={handleKeydown}/>
+<div bind:contentRect={containerRect}  class="modal" transition:modalSlide|global={{ duration }}>
+<pre>{JSON.stringify(containerRect, null, 1)}</pre>
+<button type="button" class="underline" onclick={close}>Close</button> (or press <kbd>esc</kbd>)
+</div>
+
+<style lang="postcss">
+ .modal {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  z-index: 1;
+  background-color: #fff;
+  color: #000;
+  padding: 1rem;
+ }
+  
+</style>
