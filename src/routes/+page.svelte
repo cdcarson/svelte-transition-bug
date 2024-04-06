9c9ed31e85dfb53340b75afea9349b33e42908c5
Christopher Carson <chris@nowzoo.com>
2024-04-06 18:17:54 -0500

    argh hate tabs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,39 +1,39 @@
 <script lang="ts">
-	import Modal from './Modal.svelte';
+  import Modal from './Modal.svelte';
 
-	let modalOpen = $state(false);
-	let duration: number = $state(500);
-	const close = () => {
-		modalOpen = false;
-	};
+  let modalOpen = $state(false);
+  let duration: number = $state(500);
+  const close = () => {
+    modalOpen = false;
+  };
 </script>
 
 <div class="mx-auto my-8 max-w-lg px-4">
-	<div class="space-y-4">
-		<h1 class="text-2xl">Svelte 5.0.0-next.95</h1>
+  <div class="space-y-4">
+    <h1 class="text-2xl">Svelte 5.0.0-next.95</h1>
 
-		<p>
-			Open up dev tools and look at the transition tween values (i.e. the translate y %) when you open or
-			close the modal. In Svelte 5, when the duration is less than ~500, the last value is incorrect (greater than 0 or
-			less than 100.) 
-		</p>
+    <p>
+      Open up dev tools and look at the transition tween values (i.e. the translate y %) when you
+      open or close the modal. In Svelte 5, when the duration is less than ~500, the last value is
+      incorrect (greater than 0 or less than 100.)
+    </p>
 
-		<p>
-			Transition Duration: <input type="number" class="bg-white text-black" bind:value={duration} />
-		</p>
-		<p>
-			<button class="text-5xl underline" on:click={() => (modalOpen = true)}>Open Modal</button>
-		</p>
+    <p>
+      Transition Duration: <input type="number" class="bg-white text-black" bind:value={duration} />
+    </p>
+    <p>
+      <button class="text-5xl underline" on:click={() => (modalOpen = true)}>Open Modal</button>
+    </p>
 
-		<p>To see that this bug does not happen in Svelte 4 do:</p>
-		<pre>
+    <p>To see that this bug does not happen in Svelte 4 do:</p>
+    <pre>
       git switch svelte-4
       pnpm i
       pnpm dev
     </pre>
-	</div>
+  </div>
 </div>
 
 {#if modalOpen}
-	<Modal {close} {duration} />
+  <Modal {close} {duration} />
 {/if}
--- src/routes/Modal.svelte
+++ src/routes/Modal.svelte
@@ -4,8 +4,7 @@
     close: () => void;
     duration: number;
   };
-  let { close, duration  }: P = $props();
-  
+  let { close, duration }: P = $props();
 
   const modalSlide = (_node: HTMLElement, { duration = 500 }) => {
     const mediaQuery = window.matchMedia('(prefers-reduced-motion: reduce)');
@@ -15,7 +14,7 @@
       duration: calculatedDuration,
       css: (t: number) => {
         let tyPct = 100 - sineInOut(t) * 100;
-        console.log(tyPct)
+        console.log(tyPct);
         // if (tyPct > 99.5) {
         //   tyPct = 100
         // } else if (tyPct < .5) {
@@ -25,30 +24,29 @@
       }
     };
   };
-  
-  
+
   const handleKeydown = (event: KeyboardEvent) => {
     if ('Escape' === event.key) {
       close();
     }
   };
 </script>
-<svelte:window onkeydown={handleKeydown}/>
-<div  class="modal" transition:modalSlide|global={{ duration }}>
-<button type="button" class="underline" onclick={close}>Close</button> (or press <kbd>esc</kbd>)
+
+<svelte:window onkeydown={handleKeydown} />
+<div class="modal" transition:modalSlide|global={{ duration }}>
+  <button type="button" class="underline" onclick={close}>Close</button> (or press <kbd>esc</kbd>)
 </div>
 
 <style lang="postcss">
- .modal {
-  position: fixed;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  z-index: 1;
-  background-color: #fff;
-  color: #000;
-  padding: 1rem;
- }
-  
+  .modal {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    z-index: 1;
+    background-color: #fff;
+    color: #000;
+    padding: 1rem;
+  }
 </style>
